27f5692eb2468fec7f3be4dd18b43c4ccd117843
Mike Dunn <mike@eikonomega.com>
2014-01-17 15:41:38 -0500

    Close open_file when processing is complete.

--- file_comparison_panda/file_comparison_panda.py
+++ file_comparison_panda/file_comparison_panda.py
@@ -104,7 +104,7 @@ class FileComparisonPanda(object):
                         error.filename))
             raise
         else:
-            # open_file.close()
+            open_file.close()
             return open_file
 
     def _reset_file_comparison_data(self):
